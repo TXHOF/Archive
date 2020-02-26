f44fb33c5c4d41246352cbabe9d9e3a2e73381ba
Benjamin Cobb <BDcobb333@gmail.com>
2020-02-26 16:59:06 -0600

    landing page progress

---
+++ angularTest/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LandingPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+	MatSliderModule,
+	MatToolbarModule,
+	MDBBootstrapModule.forRoot(),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
