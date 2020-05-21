e6ad60db2d1d66717896d8483dc47d37b5447902
Cameron Faubion <cameronfaubion@my.unt.edu>
2020-05-21 18:09:03 -0500

    mvn java version cleanup

--- backend/src/main/java/com/txhof/archive/config/AppConfig.java
+++ backend/src/main/java/com/txhof/archive/config/AppConfig.java
@@ -4,14 +4,12 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
-@ComponentScan("com.txhof.archive.spring")
 @PropertySource("classpath:database.properties")
 public class AppConfig {
 
