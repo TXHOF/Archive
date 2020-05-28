66cbfc1d3a977042f82c2aacefc0f3a4930bed48
George Kavalaparambil <42976315+Nixer23@users.noreply.github.com>
2020-05-28 18:30:31 -0500

    Fixed the search bar to work.

--- frontend/src/app/search-page/search-page.component.ts
+++ frontend/src/app/search-page/search-page.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, Injectable } from '@angular/core';
+import { AppService } from '../app.service';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import {
+  HttpClientJsonpModule,
+  HttpClientModule,
+  HttpClient
+} from "@angular/common/http";
+
 
 @Component({
   selector: 'app-search-page',
   templateUrl: './search-page.component.html',
   styleUrls: ['./search-page.component.css']
 })
-export class SearchPageComponent implements OnInit {
 
-  constructor() { }
+export class SearchPageComponent implements OnInit {
+  values: string = '';
+
+
+  onKey(event: any) { // without type info
+    console.log(this.values);
+  }
+
+  constructor(private app:AppService, private _route: ActivatedRoute, private _router: Router) { }
+
+  onSubmit(){
+    this._router.navigateByUrl('vrl/' + this.values);
+  }
 
   ngOnInit(): void {
+    //this._route.queryParams.subscribe(params => {
+     // this.values = params['value'];
+    //});
   }
 
 }
