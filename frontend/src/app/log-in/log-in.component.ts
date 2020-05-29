4712cf2e5441228536bc8f089e2926b3b5f42dde
theG4RYgit <piersonbeckman@my.unt.edu>
2020-05-28 19:53:23 -0500

    migrated frontendLogin changes

--- frontend/src/app/log-in/log-in.component.ts
+++ frontend/src/app/log-in/log-in.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Inject } from "@angular/core";
 import { FormControl, FormBuilder, Validators } from "@angular/forms";
+import { AppService } from '../app.service';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: "app-log-in",
@@ -9,7 +12,23 @@ import { FormControl, FormBuilder, Validators } from "@angular/forms";
 export class LogInComponent implements OnInit {
   name = new FormControl("");
 
-  constructor() {}
+   credentials = {username: '', password: ''};
 
+     error = false;
+
+  constructor(@Inject(AppService) private app: AppService, private http: HttpClient, private router: Router) {
+  }
+
+  login() {
+  var message:string = "Hello World"
+  console.log(message)
+
+    this.error=false;
+    this.app.authenticate(this.credentials, () => {  //tied to submit button on form //sent to service, which then sends to server, () is callback function
+        this.router.navigateByUrl('/');
+    });
+    this.error=true;
+    return false;
+  }
   ngOnInit(): void {}
 }
